1ae2fd615b6c84506c173f2613a3f4555be448a3
Joseph Cox <josephcox@USF-Gold-Wifi-226-6-84.laptops.usf.edu>
2018-11-27 20:31:44 -0500

    Began coding the project in java

---
+++ Traverse.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Traverse{
+    public static void main(String [] args){
+        try {
+            Scanner read = new Scanner(new File("small-input.txt")); // remember to change this to input.txt before submission
+            System.out.println("File has been read");
+            int rows = read.nextInt();
+            int columns = read.nextInt();
+            System.out.println("Rows: "+ rows + " Columns: "+columns); 
+            // Create a 2D array of Trampolines of size row and coloumns 
+            int [][] grid = new int[rows][columns]; 
+            for(int i = 0; i<rows;i++){
+                for(int j = 0; j<columns;j++){
+                    grid [i][j] = read.nextInt();
+                }
+            }
+            System.out.println("Printing out the rows and columns of grids trampolines");
+            for(int i = 0; i<rows;i++){
+                for(int j = 0; j<columns;j++){
+                    if(j%columns == 0){
+                        System.out.println(" ");
+                    }
+                    System.out.print(grid[i][j]);
+                }
+            }
+            Jim jim = new Jim(grid,rows,columns);
+            
+            jim.printJim();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+         
+
+    
+    }
+}
