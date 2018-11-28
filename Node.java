fabcfd269cb331cd1faafd481c8880ad23f227d9
Joseph Cox <josephcox@USF-Gold-Wifi-226-6-84.laptops.usf.edu>
2018-11-28 02:38:05 -0500

    Mile Stone met, still need to fix WEEAST according to patrick in Spongbob

---
+++ Node.java
@@ -0,0 +1,92 @@
+class Node{
+    /* make everything public so that everything is accessable*/
+    public int row; // the row the vertex is located 
+    public int col; // the column the vertex is located in 
+    public int value; // the value that the vertex can bounce
+    public Node north; // for the adjacency list, default it will be set to NULL
+    public Node south;
+    public Node east;
+    public Node west;
+    public static int [][] grid; // pass the int as static so that it is shared accross all instances
+    public static Node [] adjacentcyList;
+    public static int row_size;
+    public static int col_size;
+    public Node(int row,int col,int value){
+        this.row = row;
+        this.col = col;
+        this.value = value;
+        this.north = null;
+        this.south = null;
+        this.east =null;
+        this.west = null;
+
+    }
+    public void setGrid(int grid[][],int row_size,int col_size){
+        this.grid=grid;
+        this.row_size = row_size;
+        this.col_size = col_size;
+    }
+    public void printGrid(){
+        System.out.println("printing the grid");
+        for(int i=0;i<this.row_size;i++){
+            for(int j = 0;j<this.col_size;j++){
+                System.out.println(this.grid[i][j]);
+            }
+        }
+    }
+    public void setList(Node adjacentcyList[]){
+        this.adjacentcyList = adjacentcyList;
+    }
+    public void printList(){
+        System.out.println("Printing the list");
+        for(int i = 0; i<this.adjacentcyList.length;i++){
+            System.out.println(this.adjacentcyList[i].value);
+        }
+    }
+    public void getNeighbors(){
+        // North 
+        if(this.row - this.value>=0){
+            this.north = adjacentcyList[((this.row - this.value)*this.col_size)+col];
+        }
+        // South
+        if(this.row + value<this.row_size){
+            this.south = adjacentcyList[((this.row+this.value)*this.row_size)+col];
+        }
+        // East
+        if(this.col + this.value<this.col_size){
+            this.east = adjacentcyList[((this.col+this.value)+(this.row)*(this.col_size))];
+        }
+        // West
+        if(this.col - this.value<=0){
+            this.west = adjacentcyList[((this.row)*(this.col_size)-(this.col - this.value))];
+        }
+    }
+    public void printNeighbors(){
+        if(this.north == null){
+            System.out.print("North->Null | ");
+        }
+        else{
+            System.out.print("North->"+this.north.value+" | ");
+        }
+        if(this.south == null){
+            System.out.print("South->Null | ");
+        }
+        else{
+            System.out.print("South->"+this.south.value+" | ");
+        }
+        if(this.east == null){
+            System.out.print("East->Null | ");
+        }
+        else{
+            System.out.print("East->"+this.east.value+" | ");
+        }
+        if(this.west == null){
+            System.out.print("West->Null | ");
+        }
+        else{
+            System.out.print("West->"+this.west.value+" | ");
+        }
+    
+        
+    }
+}
--- Traverse.java
+++ Traverse.java
@@ -17,14 +17,41 @@ public class Traverse{
                     grid [i][j] = read.nextInt();
                 }
             }
+            Node [] adjacencyList = new Node [(rows*columns)];
+            // put everythihg int the grid into the adjacency list of size rows x columns
             System.out.println("Printing out the rows and columns of grids trampolines");
+            int k=0;
             for(int i = 0; i<rows;i++){
                 for(int j = 0; j<columns;j++){
                     if(j%columns == 0){
                         System.out.println(" ");
                     }
-                    System.out.print(grid[i][j]);
+                    System.out.print(i+j+": ");
+                    System.out.println(grid[i][j]);
+                    adjacencyList[(k)]= new Node(i, j, grid[i][j]);
+                    k++;
                 }
+            }
+            System.out.println("");
+            // printing the adjacency list
+            for(int i = 0;i<adjacencyList.length;i++){
+                System.out.println(i+": "+adjacencyList[i].value+" row: "+adjacencyList[i].row + " col "+adjacencyList[i].col);
+            }
+            adjacencyList[0].setList(adjacencyList);
+            adjacencyList[0].setGrid(grid,rows,columns);
+            System.out.println(" ");
+            adjacencyList[0].printGrid();
+            adjacencyList[0].printList();
+            for(int i =0;i<adjacencyList.length;i++){
+                if(i % 5 ==0){
+                    System.out.println("");
+                    System.out.println("");
+                }
+                System.out.println(i);
+                adjacencyList[i].getNeighbors();
+                adjacencyList[i].printNeighbors();
+                System.out.println("");
+                
             }
         } catch (IOException e) {
             e.printStackTrace();
